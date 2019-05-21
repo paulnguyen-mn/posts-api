6bd8f0ae82eef67f73266497042db5f4d60cf1a2
Hau Nguyen <paulnguyen.mn@gmail.com>
2019-05-21 21:44:17 +0700

    Allow to use custom port from env

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 3000;
  
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running')
+server.listen(port, () => {
+  console.log('JSON Server is running at port', port);
 });
